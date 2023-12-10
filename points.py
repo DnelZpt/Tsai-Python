0e8cf2f2705a57c129fdaad3bca77e76dadfeff1
DnelZpt <daniel.zapata1@utp.edu.co>
2023-12-10 04:00:46 -0500

    Working on points detection

--- points.py
+++ points.py
@@ -1,14 +1,26 @@
 import numpy as np
 import cv2
 
-path = 'shoots/'
 
+def load_image(file_path):
+    """
+    Load an image from the specified file path.
 
-def load_image(file_name):
-    image = cv2.imread(path + file_name)
+    :param file_path: A string representing the path to the image file.
+    :return: The loaded image as a grayscale image.
+    """
+    image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
     return image
 
 
 if __name__ == '__main__':
+    path = 'shoots/'
     image_01 = load_image(path + '1.jpg')
+    cv2.namedWindow('Tsai', cv2.WINDOW_NORMAL)
 
+    # Change the size for renamed window
+    cv2.resizeWindow('Tsai', 1600, 900)
+
+    cv2.imshow('Tsai', image_01)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
