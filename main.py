8dce6d21cc0a3684cacd17672f53e43edf265b62
DnelZpt <daniel.zapata1@utp.edu.co>
2023-12-10 03:25:29 -0500

    Shoots added with three panels

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+Main module for Tsai's application in camera calibration
+
+
+Daniela Buitrago, Kevin Ortega, Daniel Zapata
+MIE - UTP
+2023
+"""
+
 import numpy as np
 import cv2
+import utils
+
+images_path = '/shoots/'
 
