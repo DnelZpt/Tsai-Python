0160d69d30c9df3cb7884805a7be55bd2f9c35c1
DnelZpt <daniel.zapata1@utp.edu.co>
2023-12-11 16:58:32 -0500

    Working on main file

--- main.py
+++ main.py
@@ -9,16 +9,40 @@ MIE - UTP
 
 import numpy as np
 import basics
-from scipy.linalg import svd
+import matplotlib.pyplot as plt
 
 path = 'data/'
 corners_files = ['img_01.txt', 'img_02.txt', 'img_03.txt', 'img_04.txt', 'img_05.txt']
 
-for file in corners_files:
-    data = np.loadtxt(path + file)
-    print(file + ':')
-    print(data)
+rotation_matrices, translation_vectors = [], []
 
-    A_p = basics.get_A(data)
+for file in corners_files:
+    data_imgs = np.loadtxt(path + file)
+    print(file + ':')
+    print(data_imgs)
+
+    A_m = basics.get_A(data_imgs)
+
+    projection_matrix = basics.get_projection_matrix(A_m)
+    projection_matrix = projection_matrix.reshape(3, 4)
+    print("\nProjection matrix :\n", projection_matrix)
+
+    intrinsic, rotation, translation = basics.get_parameters(projection_matrix)
+    rotation_matrices.append(rotation)
+    translation_vectors.append(translation)
+
+    print("\nIntrinsics:\n", intrinsic)  # K matrix
+    print("\nRotation:\n", rotation)
+    print("\nTranslation:\n", translation)
+
+    error = basics.reprojection_error(projection_matrix, rotation, translation, data_imgs)
+    print("\nReprojection error is:", error)
+
+    A_p = basics.get_A(data_imgs)
+
+basics.plot_extrinsics(rotation_matrices, translation_vectors)
+plt.show()
+
+
 
 
