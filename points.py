8b725802cd204bd85037b0f6a92c9fc9daf1f63d
DnelZpt <daniel.zapata1@utp.edu.co>
2023-12-11 01:47:43 -0500

    Added corners detection function

--- points.py
+++ points.py
@@ -1,10 +1,18 @@
+"""
+Module to get corners points from the Tsai instrument
+
+Computer Vision - MIE UTP
+2023
+"""
+
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 
 def load_image(file_path):
     """
-    Load an image from the specified file path.
+    Load an image in gray scale from the specified file path.
 
     :param file_path: A string representing the path to the image file.
     :return: The loaded image as a grayscale image.
@@ -13,14 +21,62 @@ def load_image(file_path):
     return image
 
 
+def get_corner_points(image):
+    """
+    Get the corner points of an image using the goodFeaturesToTrack method.
+
+    :param image: The input image.
+    :return: The corner points of the image as a numpy array.
+    """
+    corners = cv2.goodFeaturesToTrack(image, 10, 0.01, 10)
+    return np.int64(corners)
+
+
+def show_corners(image, corners):
+    plt.figure()
+    for i in corners:
+        x, y = i.ravel()
+        cv2.circle(image, (x, y), 10, 255, -1)
+
+    plt.imshow(image_01, cmap='gray'), plt.show()
+
+
+def corners_report(corners):
+    """
+    Prints a report of the corners found in the input array. It displays the number of corners
+    and prints each corner's x and y coordinates in a tabular format.
+
+    :param corners: A NumPy array containing the corners coordinates.
+    :return: None
+    """
+
+    num_corners = corners.shape[0]
+    print(f"Number of corners: {num_corners}")
+    print('Corners found:')
+    print('   x |    y')
+    print('----------')
+    for i in corners:
+        x, y = i.ravel()
+        print('%4d | %4d' % (x, y))
+
+
 if __name__ == '__main__':
     path = 'shoots/'
     image_01 = load_image(path + '1.jpg')
-    cv2.namedWindow('Tsai', cv2.WINDOW_NORMAL)
+    image_02 = load_image(path + '2.jpg')
+    image_03 = load_image(path + '3.jpg')
+    image_04 = load_image(path + '4.jpg')
+    image_05 = load_image(path + '5.jpg')
+
+    corners_01 = get_corner_points(image_01)
+    show_corners(image_01, corners_01)
+    corners_report(corners_01)
+
+    # cv2.namedWindow('Tsai', cv2.WINDOW_NORMAL)
 
     # Change the size for renamed window
-    cv2.resizeWindow('Tsai', 1600, 900)
+    # cv2.resizeWindow('Tsai', 1600, 900)
 
-    cv2.imshow('Tsai', image_01)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('Tsai', image_01)
+    # cv2.waitKey(0)  # Close the window with 'q' key
+    # cv2.destroyAllWindows()
